fd4ed831c2893c302b0dbd98dba0b25fd41b1cdc
omid3098 <omid3098@gmail.com>
2017-08-13 10:23:21 +0430

    Session 2 added

---
+++ Advanced/Session 2/Source/Advanced - Session 2/Assets/Scripts/Editor/LevelInspector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Level))]
+public class LevelInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Level targetLevel = (Level)target;
+
+        targetLevel.xp = EditorGUILayout.IntField("XP: ", targetLevel.xp);
+        EditorGUILayout.LabelField("Level: ", targetLevel.level.ToString());
+
+		// //Update serialized object's representation.
+        // serializedObject.Update();
+		// //Apply property modifications.
+		// serializedObject.ApplyModifiedProperties();
+
+		// Implement any logic here!
+		// compare fiels or properties, use for loop etc..
+    }
+}
---
+++ Advanced/Session 2/Source/Advanced - Session 2/Assets/Scripts/Level.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public int xp;
+    public int level
+    {
+        get { return xp / 250; }
+    }
+}
