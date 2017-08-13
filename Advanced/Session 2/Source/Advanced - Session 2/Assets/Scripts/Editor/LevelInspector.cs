7f8d63a24933f0a96b67be8662344a4e528e46dd
omid3098 <omid3098@gmail.com>
2017-08-13 23:08:54 +0430

    modified after session 2

--- Advanced/Session 2/Source/Advanced - Session 2/Assets/Scripts/Editor/LevelInspector.cs
+++ Advanced/Session 2/Source/Advanced - Session 2/Assets/Scripts/Editor/LevelInspector.cs
@@ -4,19 +4,33 @@ using UnityEditor;
 [CustomEditor(typeof(Level))]
 public class LevelInspector : Editor
 {
+    private Level targetLevel;
+
+    public void OnEnable()
+    {
+        targetLevel = (Level)target;
+    }
     public override void OnInspectorGUI()
     {
-        Level targetLevel = (Level)target;
-
-        targetLevel.xp = EditorGUILayout.IntField("XP: ", targetLevel.xp);
+        // base.OnInspectorGUI();
+        DrawDefaultInspector();
+        // targetLevel.xp = EditorGUILayout.IntField("XP: ", targetLevel.xp);
         EditorGUILayout.LabelField("Level: ", targetLevel.level.ToString());
+        if (targetLevel.level > 10)
+        {
+            EditorGUILayout.LabelField("Message: ", "Baba damet garm");
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Message: ", "hanooz jooje yi");
+        }
 
-		// //Update serialized object's representation.
+        // //Update serialized object's representation.
         // serializedObject.Update();
-		// //Apply property modifications.
-		// serializedObject.ApplyModifiedProperties();
+        // //Apply property modifications.
+        // serializedObject.ApplyModifiedProperties();
 
-		// Implement any logic here!
-		// compare fiels or properties, use for loop etc..
+        // Implement any logic here!
+        // compare fiels or properties, use for loop etc..
     }
 }
