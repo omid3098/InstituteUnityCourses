8310656b2b2b9ff75b66e903c12d4c68083bcd72
omid3098 <omid3098@gmail.com>
2017-08-20 17:56:50 +0430

    Session3 in class commit

--- Advanced/Session 3/Source/Advanced - Session 3/Assets/Scripts/Editor/HeroWindow.cs
+++ Advanced/Session 3/Source/Advanced - Session 3/Assets/Scripts/Editor/HeroWindow.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEditor;
 using UnityEngine;
-
+using System.IO;
 public class HeroWindow : EditorWindow
 {
-    [MenuItem("Window/Hero Window")]
+    [MenuItem("Window/Hero Generator")]
     public static void ShowWindow()
     {
         var hwindow = EditorWindow.GetWindow(typeof(HeroWindow));
@@ -13,12 +13,14 @@ public class HeroWindow : EditorWindow
     }
 
     GUIStyle headerStyle;
-    private string heroName = "name...";
-    private bool hasSword;
-    private float swordDmg;
-    private string heroDesc = "Description...";
+    // private string heroName = "name...";
+    // private bool hasSword;
+    // private float swordDmg;
+    // private string heroDesc = "Description...";
+    private Hero hero;
     void OnEnable()
     {
+        hero = ScriptableObject.CreateInstance<Hero>();
         headerStyle = new GUIStyle();
         headerStyle.fontSize = 20;
         headerStyle.fontStyle = FontStyle.Bold;
@@ -30,21 +32,30 @@ public class HeroWindow : EditorWindow
         EditorGUILayout.LabelField("Hero Generator", headerStyle);
         EditorGUILayout.Space();
 
-        heroName = EditorGUILayout.TextField("Name:", heroName);
+        hero.heroName = EditorGUILayout.TextField("Name:", hero.heroName);
 
         EditorGUILayout.BeginHorizontal();
-        heroDesc = EditorGUILayout.TextField("Desc:", heroDesc);
-
+        hero.heroDesc = EditorGUILayout.TextField("Desc:", hero.heroDesc);
         EditorGUILayout.EndHorizontal();
 
-        hasSword = EditorGUILayout.BeginToggleGroup("Has Sword:", hasSword);
-        // hasSword = EditorGUILayout.Toggle("Has Sword:", hasSword);
-        swordDmg = EditorGUILayout.Slider("Sword Dammage", swordDmg, 0, 10);
+        hero.hasSword = EditorGUILayout.BeginToggleGroup("Has Sword:", hero.hasSword);
+        hero.swordDammage = EditorGUILayout.Slider("Sword Dammage", hero.swordDammage, 0, 10);
         EditorGUILayout.EndToggleGroup();
 
         if (GUILayout.Button("Generate hero"))
         {
             Debug.Log("hero was generated!");
+            string _directory = "Assets/" + "heroes/";
+            string _fileName = hero.heroName + ".asset";
+            if (!Directory.Exists(_directory))
+            {
+                Directory.CreateDirectory(_directory);
+            }
+            if (File.Exists(_directory + _fileName))
+            {
+                File.Delete(_directory + _fileName);
+            }
+            AssetDatabase.CreateAsset(hero, _directory + _fileName);
         }
     }
 }
