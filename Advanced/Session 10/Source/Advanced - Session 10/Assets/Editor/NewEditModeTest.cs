a52d6762e627c4cdcf04a5616c5f74034ee71575
omid3098 <omid3098@gmail.com>
2017-10-15 17:54:59 +0330

    Session 10 Advance-basic

---
+++ Advanced/Session 10/Source/Advanced - Session 10/Assets/Editor/NewEditModeTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewEditModeTest
+{
+
+    [Test]
+    public void NewEditModeTestSimplePasses()
+    {
+        // Use the Assert class to test conditions.
+        var q = 10;
+        Assert.AreEqual(q, 9);
+    }
+}
